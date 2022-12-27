9bc6faef9da9000e066c868e8137e5a40de67120
Martin <prsmtn@gmail.com>
2022-12-27 20:28:31 +0100

    f

--- Ypsilon2/src/components/HomePage.vue
+++ Ypsilon2/src/components/HomePage.vue
@@ -1,10 +1,9 @@
-// App.vue
 <template>
   <div class="no-scroll">
     <MyRectangle style="z-index: -1;"/>
     
-    <img src="./assets/logo.jpg" style="width: 120px; height: 120px; position: relative; left: 60px; top: 20px; border-radius: 15px; box-shadow: 0px 0px 10px 5px rgba(0, 0, 0,0.1);">
-    <img src="./assets/profilepic.png" style="width: 60px; height: 60px; position: relative; left: 1200px; top:-50px; border-radius: 60px; ">
+    <img src="../assets/logo.jpg" style="width: 120px; height: 120px; position: relative; left: 60px; top: 20px; border-radius: 15px; box-shadow: 0px 0px 10px 5px rgba(0, 0, 0,0.1);">
+    <img src="../assets/profilepic.png" style="width: 60px; height: 60px; position: relative; left: 1200px; top:-50px; border-radius: 60px; ">
     <p style="position: relative; left: 1180px; top:-110px;" >NOME PROFILO</p>
     <p></p>
     <HomePage label="HomePage" style="margin-top:120px; margin-left: 45px;"  />
@@ -49,11 +48,11 @@
 
 <script>
 
-import HomePage from './components/My-Bottone.vue'
-import RicercaCorsi from './components/My-Bottone.vue'
-import AreaProfilo from './components/My-Bottone.vue'
-import MyRectangle from './components/MyRectangle.vue'
-import Corso from './components/My-Corso.vue'
+import HomePage from './My-Bottone.vue'
+import RicercaCorsi from './My-Bottone.vue'
+import AreaProfilo from './My-Bottone.vue'
+import MyRectangle from './MyRectangle.vue'
+import Corso from './My-Corso.vue'
 
 
 
