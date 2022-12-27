20c0eaefe4820ad0ebd80065cd32cf7b5cff372f
Andrea Richichi <andrea.richichi@studenti.unitn.it>
2022-12-27 19:06:49 +0100

    front-end

---
+++ Ypsilon2/src/HomePage.vue
@@ -0,0 +1,77 @@
+// App.vue
+<template>
+  <div class="no-scroll">
+    <MyRectangle style="z-index: -1;"/>
+    
+    <img src="./assets/logo.jpg" style="width: 120px; height: 120px; position: relative; left: 60px; top: 20px; border-radius: 15px; box-shadow: 0px 0px 10px 5px rgba(0, 0, 0,0.1);">
+    <img src="./assets/profilepic.png" style="width: 60px; height: 60px; position: relative; left: 1200px; top:-50px; border-radius: 60px; ">
+    <p style="position: relative; left: 1180px; top:-110px;" >NOME PROFILO</p>
+    <p></p>
+    <HomePage label="HomePage" style="margin-top:120px; margin-left: 45px;"  />
+    <p></p>
+
+    <RicercaCorsi label="Ricerca corsi" style="margin-top:70px;  margin-left: 45px;" />
+    <p></p>
+  
+    <AreaProfilo label="Profilo Personale" style="margin-top:70px;  margin-left: 45px;" />
+
+    <p></p>
+
+    <p style="position: absolute; left: 360px; top:130px;font-size: x-large; font-weight: bold;" >I tuoi Corsi preferiti:</p>
+    
+    <Corso style="position: absolute; left:400px; top: 200px;"/>
+    <Corso style="position: absolute; left:700px; top: 200px;"/>
+    <Corso style="position: absolute; left:1000px; top: 200px;"/>
+
+    
+    <p style="position: absolute; left: 360px; top:430px;font-size: x-large; font-weight: bold;" >I Corsi più votati:</p>
+    <Corso style="position: absolute; left:400px; top: 500px;"/>
+    <Corso style="position: absolute; left:700px; top: 500px;"/>
+    <Corso style="position: absolute; left:1000px; top: 500px;"/>
+
+    
+  </div>
+
+ 
+
+</template>
+
+<style>
+.no-scroll {
+  overflow: hidden;
+}
+
+
+
+
+
+</style>
+
+<script>
+
+import HomePage from './components/My-Bottone.vue'
+import RicercaCorsi from './components/My-Bottone.vue'
+import AreaProfilo from './components/My-Bottone.vue'
+import MyRectangle from './components/MyRectangle.vue'
+import Corso from './components/My-Corso.vue'
+import request from 'postman-request';
+
+// Eseguire una richiesta GET
+request.get("http://localhost:8080/api/users/63a5c5989075ad6f17d16b99/courses", (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(response.statusCode, body);
+  }
+});
+
+
+
+
+export default {
+  components: {
+    HomePage, RicercaCorsi,AreaProfilo,MyRectangle,Corso
+  }
+}
+
+</script>
---
+++ Ypsilon2/src/components/My-Corso.vue
@@ -0,0 +1,41 @@
+<template>
+    <table class="gray-table" style="border-radius: 10px; width: 220px; height: 120px;">
+      <tr>
+        <td style="border-radius: 10px; text-align: center">Nome Professore</td>
+        <td style="border-radius: 10px;  display: flex; align-items: center; justify-content: center;">
+            <button style="width: 40px; height: 40px; border-radius: 60px; display: flex; align-items: center; justify-content: center;">    
+                <img src="./like.png" style="width: 36px; height:36px; border-radius: 60px; ">
+            </button>
+        </td>
+      </tr>
+      <tr style="height: 80px; ">
+        <td colspan="2" style="font-size: large; border-radius: 10px; text-align: center;">Nome Corso</td>
+      </tr>
+      <tr style="height: 40px;">
+        <td colspan="2" style="font-size: large; border-radius: 10px; text-align: center;">valutazione</td>
+    </tr>
+    </table>
+  </template>
+
+<style>
+.gray-table {
+    background-color: #D9D9D9;
+    border: 2px solid gray;
+  }
+  
+  .gray-table td {
+    border: 2px solid white;
+  }
+</style> 
+  
+  <script>
+  export default {
+    props: {
+      testo: {
+        type: String,
+        required: true
+      }
+    }
+  }
+  </script>
+  
