8bb8e15cae4837bf2dcc671660932becdc2c6c27
Martin <prsmtn@gmail.com>
2022-12-27 21:36:17 +0100

    f

--- Ypsilon2/src/components/HomePage.vue
+++ Ypsilon2/src/components/HomePage.vue
@@ -6,20 +6,20 @@
     <img src="../assets/profilepic.png" style="width: 60px; height: 60px; position: relative; left: 1200px; top:-50px; border-radius: 60px; ">
     <p style="position: relative; left: 1180px; top:-110px;" >NOME PROFILO</p>
     <p></p>
-   
-
+    
+    
     <p style="position: absolute; left: 360px; top:130px;font-size: x-large; font-weight: bold;" >I tuoi Corsi preferiti:</p>
     
     <Corso style="position: absolute; left:400px; top: 200px;"/>
     <Corso style="position: absolute; left:700px; top: 200px;"/>
     <Corso style="position: absolute; left:1000px; top: 200px;"/>
-
     
     <p style="position: absolute; left: 360px; top:430px;font-size: x-large; font-weight: bold;" >I Corsi più votati:</p>
     <Corso style="position: absolute; left:400px; top: 500px;"/>
     <Corso style="position: absolute; left:700px; top: 500px;"/>
     <Corso style="position: absolute; left:1000px; top: 500px;"/>
-
+    
+    <button @click="first_api()" type="button">prova Api</button>
     
   </div>
 
@@ -32,10 +32,6 @@
   overflow: hidden;
 }
 
-
-
-
-
 </style>
 
 <script>
@@ -43,14 +39,26 @@
 
 import MyRectangle from './MyRectangle.vue'
 import Corso from './My-Corso.vue'
-
-
-
+import axios from 'axios'
 
 export default {
   components: {
     MyRectangle,Corso
+  },
+  data(){
+    return {
+      joke: ''
+    }
+  },
+  methods: {
+    async first_api(){
+      let config = {
+        header: {
+          'Accept': 'application.json'}
+      }
+      const joke = await axios.get('http://localhost:8080/api/users/63a5c5989075ad6f17d16b99/courses', config);
+      this.joke = joke.data
+    }
   }
 }
-
 </script>
