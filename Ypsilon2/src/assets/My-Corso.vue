dfbd7c734df775343429beafc517cf1279c8ca4c
Andrea Richichi <andrea.richichi@studenti.unitn.it>
2022-12-28 18:16:59 +0100

    ike

--- Ypsilon2/src/assets/My-Corso.vue
+++ Ypsilon2/src/assets/My-Corso.vue
@@ -4,7 +4,7 @@
       <tr v-if="item">
         <td style="border-radius: 10px; text-align: center;">{{` ${item.utente_id["nome"]}  ${item.utente_id["cognome"]} `}}</td>
         <td style="border-radius: 10px; display: flex; align-items: center; justify-content: center;">
-          <button style="width: 40px; height: 40px; border-radius: 60px; display: flex; align-items: center; justify-content: center;">    
+          <button style="width: 40px; height: 40px; border-radius: 60px; display: flex; align-items: center; justify-content: center;" @click="handleClick">    
             <img v-if="item.is_favourite" src="../assets/like.png" style="width: 36px; height:36px; border-radius: 60px; ">
             <img v-else src="../assets/likeoff.png" style="width: 36px; height:36px; border-radius: 60px; ">
           </button>
@@ -37,6 +37,7 @@
 
   <script>
   import { object } from 'webidl-conversions';
+  import axios from 'axios'
 
   export default {
     props: {
@@ -44,7 +45,25 @@
         type: object,
         required: true
       }
+    },
+    methods: {
+  handleClick() {
+    
+      let base_api = 'http://localhost:8080/api/';
+
+      const data={
+        corso_id: this.item.id,
+        utente_id:'63a5c5989075ad6f17d16b99'
+      }
+        
+    if (!this.item.is_favourite) {
+      axios.post(base_api+'favourite_courses/', data);//lista users
+    } else {
+      axios.delete(base_api+'favourite_courses');//lista corsi
     }
   }
+}
+  }
+  
   </script>
   
