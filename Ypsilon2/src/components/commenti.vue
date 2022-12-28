ce0897f105a0fa6dbd21ace0b153b5f3825bd4a6
Leonardo Bridi <leonardo.bridi00@gmail.com>
2022-12-28 11:28:53 +0100

    commenti

---
+++ Ypsilon2/src/components/commenti.vue
@@ -0,0 +1,147 @@
+<template>
+    <table class="comments">
+      <tr v-for="comment in sortedComments" :key="comment.id">
+        <td class="comment-author">{{ comment.user }}:</td>
+        <td class="comment-text">{{ comment.text }}</td>
+        <td class="comment-likes-dislikes">
+          <img src="path/to/like-image" @click="likeComment(comment)" /> {{ comment.likes }}
+          <img src="path/to/dislike-image" @click="dislikeComment(comment)" /> {{ comment.dislikes }}
+        </td>
+        <td class="comment-actions">
+          <button v-if="comment.user === currentUser" @click="deleteComment(comment)">Elimina</button>
+        </td>
+      </tr>
+    </table>
+    <form @submit.prevent="addComment" class="add-comment-form">
+      <textarea v-model="newCommentText" placeholder="Scrivi il tuo commento" style="width: 500px; height: 100px;"></textarea>
+      <button type="submit">Invia</button>
+    </form>
+    <div class="sort-menu">
+      <button
+        v-bind:class="{ active: sortBy === 'chronological' }"
+        @click="sortBy = 'chronological'"
+      >
+        Cronologico
+      </button>
+      <button
+        v-bind:class="{ active: sortBy === 'likes' }"
+        @click="sortBy = 'likes'"
+      >
+        Mi piace
+      </button>
+    </div>
+  </template>
+  <script>
+  export default {
+    data() {
+      return {
+        comments: [],
+        currentUser: '',
+        newCommentText: '',
+        sortBy: 'Cronologico',
+        sortMenuOpen: false,
+      };
+    },
+    computed: {
+    sortedComments() {
+      const comments = this.comments.slice(); // Crea una copia dell'array dei commenti
+      if (this.sortBy === 'chronological') {
+        return comments.sort((a, b) => b.timestamp - a.timestamp); // Restituisci i commenti ordinati in base alla data di pubblicazione
+      } else if (this.sortBy === 'likes') {
+        return comments.sort((a, b) => b.likes - a.likes); // Restituisci i commenti ordinati in base al numero di mi piace
+  } else {
+  return comments; // Restituisci i commenti senza ordinarli
+  }
+  }
+  },
+    
+    methods: {
+      addComment() {
+        this.comments.push({
+          id: Date.now(),
+          text: this.newCommentText,
+          user: 'Leonardo_Bridi', // sostituire con il nome dell'utente corrente
+          likes: 0,
+          dislikes: 0,
+          voted: null
+        })
+        this.newCommentText = ''
+      },
+      deleteComment(comment) {
+        const index = this.comments.indexOf(comment)
+        this.comments.splice(index, 1)
+      },
+      likeComment(comment) {
+        if (comment.voted === 'like') {
+          // L'utente sta cambiando il proprio voto da "mi piace" a "non votato"
+          comment.likes--
+          comment.voted = null
+        } else if (comment.voted === 'dislike') {
+          // L'utente sta cambiando il proprio voto da "non mi piace" a "mi piace"
+          comment.dislikes--
+          comment.likes++
+          comment.voted = 'like'
+        } else {
+          // L'utente sta votando per la prima volta il commento con "mi piace"
+          comment.likes++
+          comment.voted = 'like'
+        }
+      },
+      dislikeComment(comment) {
+        if (comment.voted === 'dislike') {
+          // L'utente sta cambiando il proprio voto da "non mi piace" a "non votato"
+          comment.dislikes--
+          comment.voted = null
+         } else if (comment.voted === 'like') {
+  // L'utente sta cambiando il proprio voto da "mi piace" a "non mi piace"
+  comment.likes--
+  comment.dislikes++
+  comment.voted = 'dislike'
+  } else {
+  // L'utente sta votando per la prima volta il commento con "non mi piace"
+  comment.dislikes++
+  comment.voted = 'dislike'
+  }
+  }
+  }
+  }
+  
+  </script>
+  
+  <style>
+  .comments {
+    width: 500px;
+    height: 300px;
+    overflow: auto; /* permette di visualizzare solo il contenuto della tabella e di scorrere il resto */
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+  
+  .comment {
+    display: flex;
+    align-items: flex-start;
+    margin-bottom: 20px;
+    border-bottom: 1px solid #ddd;
+  }
+  
+  .comment-author {
+    font-weight: bold;
+    margin-right: 10px;
+  }
+  
+  .comment-text {
+    flex: 1;
+  }
+  
+  .comment-likes-dislikes {
+    margin-left: 10px;
+  }
+  
+  .comment-actions {
+    margin-left: 10px;
+  }
+  </style>
+  
+  
+  
